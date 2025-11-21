ca7b383de0704650b26bc2f065e838dbb7da4721
Lakshman Pal <lakshman.leelija@gmail.com>
2025-11-21 10:26:16 +0530

    update soft delete in user mail history

--- app/Models/UserMailHistory.php
+++ app/Models/UserMailHistory.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserMailHistory extends Model
 {
+    use HasFactory;
+    use SoftDeletes;
     protected $table = 'user_mail_history';
 
     protected $fillable = [
