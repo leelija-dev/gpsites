85dd35c9a9a1935e4464ff7743234b4c57c2904b
Lakshman Pal <lakshman.leelija@gmail.com>
2025-11-28 13:27:30 +0530

    update blog api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,10 +12,11 @@ class HomeController extends Controller
     $response = Http::get(env('API_BASE_URL') .'/api/niches');
        if ($response->successful()) {
         // If your API returns paginated response
-        $niches_data = $response->json()['data'] ?? [];
+        $niches_data = $response->json() ?? [];
     } else {
         $niches_data = [];
     }
+    // print_r($response);die;
     $plans = Plan::with('features')
         ->where('is_active', true)
         ->orderBy('price', 'asc')
