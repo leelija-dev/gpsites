6460347dedd0d55451b085aa121fce1ee2d5f17b
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2025-12-05 17:51:56 +0530

    admin and plan seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(AdminSeeder::class);
-        // $this->call(PlanSeeder::class);
+        $this->call(PlanSeeder::class);
     }
 }
